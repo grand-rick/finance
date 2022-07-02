bb002672e3f725b8060cbdd9828e46dc3d93335e
Patrick Kabuga <kabugap02@gmail.com>
2022-07-03 00:47:31 +0300

    added Procfile

--- app.py
+++ app.py
@@ -23,8 +23,11 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+uri = os.getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://")
 
+db = SQL(postgres://uiqriapemjrvop:c74dd67063fd2c937a0b7898f6d2eb9575bba312b2bf7a2195bd5c52c4aa3c0b@ec2-23-23-151-191.compute-1.amazonaws.com:5432/d670afivjtqfrf)
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
     raise RuntimeError("API_KEY not set")
